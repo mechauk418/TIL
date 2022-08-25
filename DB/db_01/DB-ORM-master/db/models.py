aef495ba8675c86f9e15e5f817f06fb7cc48529b
mechauk418 <mechauk418@gmail.com>
2022-08-25 17:46:08 +0900

    0825 실습

--- DB/db_01/DB-ORM-master/db/models.py
+++ DB/db_01/DB-ORM-master/db/models.py
@@ -1,12 +1,18 @@
 import sys
 from django.db import models
 
-
 class Director(models.Model):
-    name = models.TextField(max_length=30)
+    name = models.TextField()
     debut = models.DateTimeField()
-    country = models.TextField(max_length=30)
+    country = models.TextField()
 
 class Genre(models.Model):
-    title = models.TextField(max_length=30)
+    title = models.TextField()
 
+class Movie(models.Model):
+    director = models.ForeignKey(Director,on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre,on_delete=models.CASCADE)
+    title = models.TextField()
+    opening_date = models.DateField()
+    running_time = models.IntegerField()
+    screening = models.BooleanField()
